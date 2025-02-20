eae5415ec5b068e8d063127df2f4604eecf0fcf9
Chukwu Emmanuel <eechukwu@gmail.com>
2025-02-20 13:19:03 +0000

    Creating a CI Pipeline

---
+++ terraform/k8s.tf
@@ -0,0 +1,19 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+
+  cluster_name                   = local.name
+  cluster_version                = local.cluster_version
+  cluster_endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  tags = local.tags
+}
