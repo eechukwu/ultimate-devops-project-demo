3c489db5f487550fca21486c5aaf9264c3999d3a
Chukwu Emmanuel <eechukwu@gmail.com>
2025-02-20 23:19:39 +0000

    Creating a CI Pipeline

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -3,31 +3,14 @@ module "eks" {
   version = "~> 19.0"
 
   cluster_name                   = local.name
-  cluster_version                = local.cluster_version
+  cluster_version               = local.cluster_version
   cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # Manage aws-auth configmap
-  manage_aws_auth_configmap = true
-  aws_auth_roles = [
-    {
-      rolearn  = aws_iam_role.eks_admin_role.arn
-      username = "eks-admin"
-      groups   = ["system:masters"]
-    },
-    {
-      rolearn  = aws_iam_role.eks_developer_role.arn
-      username = "eks-developer"
-      groups   = ["developer"]
-    },
-    {
-      rolearn  = aws_iam_role.eks_readonly_role.arn
-      username = "eks-readonly"
-      groups   = ["readonly"]
-    }
-  ]
+  # Temporarily disable aws-auth management
+  manage_aws_auth_configmap = false
 
   eks_managed_node_groups = {
     general = {
